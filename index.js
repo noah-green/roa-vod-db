c6281f326fa6accf580af42c3dcfecd417cdde78
noah-green <ngreen612@gmail.com>
2021-01-19 16:54:05 -0500

    changed port

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const port = process.env.PORT || 5000;
+
 const app = express();
 
 app.get('/', async (req, res) => {
     res.json({hello: 'hello'});
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("listening on 3000");
 });
