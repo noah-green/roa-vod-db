0d3248cca00d94e5c776fca2acc072bcf71fb53f
noah-green <ngreen612@gmail.com>
2021-01-19 19:07:59 -0500

    incremental commit

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
-
-const port = process.env.PORT || 5000;
-
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
     res.json({hello: 'hello'});
 });
 
 app.listen(port, () => {
-    console.log("listening on 3000");
+    console.log(`listening on ${port}`);
 });
